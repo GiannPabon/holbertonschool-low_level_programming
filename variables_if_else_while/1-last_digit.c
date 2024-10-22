ebb63162b34591c5a2ce7a8af58401dec90ffaa2
Giann Pabon <giannalberto2@outlook.com>
2024-10-22 14:18:54 -0400

    Prints the last digit in int n

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ last_digit = n % 10; /*divides n by 10 to get last digit*/
 
 if (last_digit > 5)
 {
-printf("Last digit of %d is %d\n", n, last_digit);
+printf("Last digit of %d is %d\n and is greater than 5", n, last_digit);
 }
 else if (last_digit == 0)
 {
-printf("Last digit of %d is %d\n", n, last_digit);
+printf("Last digit of %d is %d\n and is 0", n, last_digit);
 }
 else
 {
-printf("Last digit of %d is %d\n", n, last_digit);
+printf("Last digit of %d is %d\n and is less than 6 and not 0", n, last_digit);
 }
 return (0);
 }

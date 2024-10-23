20d54342c1ca1f1eaec9a4830c9d76422d5a6370
Giann Pabon <giannalberto2@outlook.com>
2024-10-22 22:27:34 -0400

    Print alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Prints the entire alphabet
+ *Return: (0) success
+ */
+int main(void)
+{
+int letter;
+
+    /* For loop to print the alphabet from A to Z */
+for (letter < 122; letter > 97; letter++)
+{
+putchar(letter); /*The zero coverts digit to ASCII equivalent*/
+}
+putchar('\n');
+return (0);
+}

0a70ead27e2a74c9a7290743204bb18bab17ea5d
Giann Pabon <giannalberto2@outlook.com>
2024-10-24 16:53:33 -0400

    Check for lowercase characters

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Output 1 if char is c if not it will print 0
+ * c - will hold an int
+ * Return: 0 success
+ *
+ */
+
+
+int _islower(int c)
+{
+if (c == 97)
+	return (1);
+else
+	return (0);
+}
+

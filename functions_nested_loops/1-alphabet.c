a1cccddf9e08807bdebe83ef159b33942e65afc6
Giann Pabon <giannalberto2@outlook.com>
2024-10-24 15:33:59 -0400

    Print lowercase alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - will print the alphabet in lower cases
+ *
+ * Return: 0 success
+ *
+ */
+
+int main(void)
+{
+int letter;
+
+for (letter = 97; letter <= 122; letter++)
+	_putchar(letter);
+
+_putchar('\n');
+
+return (0);
+}
+
+

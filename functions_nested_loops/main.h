74f85fb78cb5127403d6e054c7745f20e820d9a5
Giann Pabon <giannalberto2@outlook.com>
2024-10-24 10:58:49 -0400

    Add main.h

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H   /* Header guard to prevent multiple inclusions */
+#define MAIN_H
+
+/* Function prototypes */
+int _putchar(char c);		/* Prototype for _putchar */
+void print_alphabet(void);	/* Prototype for print_alphabet */
+int _islower(int c);		/* Prototype for _islower */
+int _isalpha(int c);		/* Prototype for _isalpha */
+int print_sign(int n);		/* Protoype for print_sign*/
+int _abs(int);			/* Prototype for _abs(int) */
+int print_last_digit(int);	/* Prototype for print_last_digit */
+void jack_bauer(void);		/* Prototype for jack_bauer*/
+void times_table(void);		/* Prototype for times_table */
+int add(int, int);		/* Protype for add */
+void print_to_98(int n);	/* Protoype for print_to_98  */
+
+
+#endif /* MAIN_H */

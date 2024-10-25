05c4b3430d39346bcd4d6f0dcfc0fb70019365e0
Giann Pabon <giannalberto2@outlook.com>
2024-10-24 23:15:04 -0400

    Output absolute value of n

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - will output the absolute value of n
+ * @n: will hold an int
+ * Return: 0 success
+ *
+ */
+
+int _abs(int n)
+{
+
+if (n < 0)
+return (-n);
+
+else
+return (n);
+
+}

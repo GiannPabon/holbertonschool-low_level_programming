90d9d8b0d85627f34a1f4f491ea396e16d047b66
Giann Pabon <giannalberto2@outlook.com>
2024-10-24 21:43:14 -0400

    Checks all characters

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - Output 1 if variable is char if not it will print 0
+ * @c: will hold an int
+ * Return: 0 success
+ *
+ */
+
+
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 90)||(c >= 97 && c <= 122))
+        return (1);
+else
+        return (0);
+}

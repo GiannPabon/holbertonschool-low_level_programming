a0862e2603f49b03fc77bd1dd6ae41f8239a72a1
Giann Pabon <giannalberto2@outlook.com>
2024-10-27 22:45:45 -0400

    Pritns time table till 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,35 +5,35 @@
  */
 void times_table(void)
 {
-    int row, col, product;
+int row, col, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (col = 0; col <= 9; col++)
-        {
-            product = row * col;
+for (row = 0; row <= 9; row++)
+{
+for (col = 0; col <= 9; col++)
+{
+product = row * col;
 
-            if (col > 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-                /* Add extra space if product is a single digit */
-                if (product < 10)
-                    _putchar(' ');
-            }
+if (col > 0)
+{
+_putchar(',');
+_putchar(' ');
+/* Add extra space if product is a single digit */
+if (product < 10)
+_putchar(' ');
+}
 
-            /* Print each digit of the product */
-            if (product >= 10)
-            {
-                _putchar((product / 10) + '0'); /* Print tens digit */
-                _putchar((product % 10) + '0'); /* Print ones digit */
-            }
-            else
-            {
-                _putchar(product + '0');
-            }
-        }
-        _putchar('\n');
-    }
+/* Print each digit of the product */
+if (product >= 10)
+{
+_putchar((product / 10) + '0'); /* Print tens digit */
+_putchar((product % 10) + '0'); /* Print ones digit */
+}
+else
+{
+_putchar(product + '0');
+}
+}
+_putchar('\n');
+}
 }
 

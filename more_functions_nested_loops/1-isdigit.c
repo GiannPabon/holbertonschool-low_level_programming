cf08385a30ff384ac0a4a09651479a133de95c94
Giann Pabon <giannalberto2@outlook.com>
2024-10-28 10:53:55 -0400

    Return 1 if digit 0-9

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - main function
+ * @c: main variable
+ *Return: success
+ *
+ */
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+
+}

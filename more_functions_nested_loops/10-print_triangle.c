9cf911f2b6dcc307e44c7367ac10c40443a6938e
Giann Pabon <giannalberto2@outlook.com>
2024-10-28 22:02:25 -0400

    Print Triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using the '#' character
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int row, space, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (space = 1; space <= size - row; space++)
+			{
+				_putchar(' ');
+			}
+			for (hash = 1; hash <= row; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+

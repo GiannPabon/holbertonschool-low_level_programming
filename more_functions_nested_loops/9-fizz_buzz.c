dcacd3f40bb8ddd9387767d925c0f4c13d721bb3
Giann Pabon <giannalberto2@outlook.com>
2024-10-28 21:41:32 -0400

    Fizz Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,47 @@
+#include "main.h"
+void print_number(int n)
+{
+	if (n >= 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else
+		{
+			print_number(i);
+		}
+		if (i != 100)
+			_putchar(' ');
+	}
+	_putchar('\n');
+	return (0);
+}

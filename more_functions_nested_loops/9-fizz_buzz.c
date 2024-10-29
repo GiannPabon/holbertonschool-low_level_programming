c8b12d19153b066a767478e57b0f4e024ab3eafa
Giann Pabon <giannalberto2@outlook.com>
2024-10-28 21:53:58 -0400

    Fizz Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,22 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
+/**
+ * print_number - prints an integer using _putchar
+ * @n: The integer to print
+ */
 void print_number(int n)
 {
 	if (n >= 10)
@@ -13,9 +24,15 @@ void print_number(int n)
 	_putchar((n % 10) + '0');
 }
 
+/**
+ * main - prints numbers from 1 to 100 with Fizz, Buzz, and FizzBuzz substitutions
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)

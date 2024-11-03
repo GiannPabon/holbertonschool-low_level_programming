c0c27e2001756d52dad28135bd079675858b9448
Giann Pabon <giannalberto2@outlook.com>
2024-11-03 15:18:11 -0400

    Copy string to the buffer dest

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,27 +8,25 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
 
-    /* Determine sign and move to the first number */
-    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
 
-    /* Process digits and check for overflow */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        if (result > (2147483647 - (s[i] - '0')) / 10)
-            return (sign == 1 ? 2147483647 : -2147483648);
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > (2147483647 - (s[i] - '0')) / 10)
+			return (sign == 1 ? 2147483647 : -2147483648);
 
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 
-    return result * sign;
+	return result * sign;
 }

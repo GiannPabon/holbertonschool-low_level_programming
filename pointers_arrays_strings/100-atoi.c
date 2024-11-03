ac706b9034345ae9787d94371be969a900918ef0
Giann Pabon <giannalberto2@outlook.com>
2024-11-03 15:16:54 -0400

    Copy string to the buffer dest

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,56 +1,34 @@
 #include "main.h"
+
 /**
- * _atoi - Convert a string to an integer
- * @s: string to convert
- * Return: return r if negative, return sum if positive.
+ * _atoi - converts a string to an integer
+ * @s: input string
+ *
+ * Return: the integer value of the string, or 0 if no numbers are found
  */
 int _atoi(char *s)
 {
-	int i, j, n, convert, mul, sum, r;
+    int i = 0;
+    int sign = 1;
+    int result = 0;
 
-	char *d, *a;
+    /* Determine sign and move to the first number */
+    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-	n = 0;
-	mul = 1;
-	convert = 0;
-	sum = 0;
+    /* Process digits and check for overflow */
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        if (result > (2147483647 - (s[i] - '0')) / 10)
+            return (sign == 1 ? 2147483647 : -2147483648);
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == '-')
-		{
-			n++;
-		}
-		if (s[i] > 47 && s[i] < 58)
-		{
-			d = s + i;
-			break;
-		}
-	}
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
 
-	for (j = 0; d[j] != '\0'; j++)
-	{
-		if (d[j] < 48 || d[j] > 57)
-		{
-			d[j] = '\0';
-			break;
-		}
-	}
-	a = d + (j - 1);
-
-	while (a != d - 1)
-	{
-		convert = *a - 48;
-		sum += convert * mul;
-		mul = mul * 10;
-		a--;
-	}
-
-	if (!n % 2 == 0)
-	{
-		r = sum * -1;
-		return (r);
-	}
-
-	return (sum);
+    return result * sign;
 }

1bc10247a69938f8dfaabc3cd45ddf520c471a66
Giann Pabon <giannalberto2@outlook.com>
2024-11-03 21:35:05 -0400

    capitalizes all words of a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: input string
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int capitalize_next = 1;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	while (str[i] != '\0')
+	{
+		if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] -= 'a' - 'A';
+			capitalize_next = 0;
+		}
+
+		/* Check if the current character is a separator */
+		for (int j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
+			{
+				capitalize_next = 1;
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
+

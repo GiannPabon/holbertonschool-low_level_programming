9a6d83406b46ac8e80e50436bde0816f36452ccb
Giann Pabon <giannalberto2@outlook.com>
2024-11-10 00:24:30 -0600

    Implements byte search in string

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - Finds the first matching byte in a string
+ * @s: The input string to search
+ * @accept: The set of bytes to match
+ *
+ * Return: Pointer to the first match in `s`, or NULL if none found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		char *a = accept;
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
+

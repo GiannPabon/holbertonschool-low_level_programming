b5fb8577982004d5b9a8d02a59600bc500708cdb
Giann Pabon <giannalberto2@outlook.com>
2024-11-16 23:23:42 -0400

    Return the natural square root of a number

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt_helper(n, 0));
+	return _sqrt_helper(n, 0);
 }
 
 /**
@@ -24,6 +24,6 @@ int _sqrt_helper(int n, int guess)
 		return (guess);
 	if (guess * guess > n)
 		return (-1);
-	return (_sqrt_helper(n, guess + 1));
+	return _sqrt_helper(n, guess + 1);
 }
 

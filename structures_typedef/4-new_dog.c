2a84e5be82a58ec17d9f30a3758393248965766e
Giann Pabon <giannalberto2@outlook.com>
2024-11-18 09:48:03 -0400

    Creates a new dog function

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strdup - Duplicates a string
+ * @str: The string to duplicate
+ * Return: Pointer to the duplicated string or NULL if failed
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, len = 0;
+	if (str == NULL)
+		return NULL;
+	while (str[len])
+		len++;
+	dup = malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return NULL;
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+	dup[len] = '\0';
+	return dup;
+}
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: Pointer to the new dog or NULL if failed
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return NULL;
+	dog->name = _strdup(name);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return NULL;
+	}
+	dog->owner = _strdup(owner);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return NULL;
+	}
+	dog->age = age;
+	return dog;
+}
+

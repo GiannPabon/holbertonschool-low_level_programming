dc23b86c25fcd01c8c327629c72cbbe2e85e0917
Giann Pabon <giannalberto2@outlook.com>
2024-11-22 15:04:54 -0400

    main 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,26 +1,33 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - Performs simple arithmetic operations.
- * @argc: Argument count.
- * @argv: Argument vector.
- * Return: 0 on success, exits with specific codes on error.
+ * main - Entry point for the program.
+ * @argc: Number of arguments.
+ * @argv: Array of argument strings.
+ *
+ * Return: 0 on success, or exits with appropriate status code on error.
  */
+
 int main(int argc, char *argv[])
 {
+	int num1;
+	int num2;
+	int result;
+	int (*opr)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	opr = get_op_func(argv[2]);
+	if (opr == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	result = opr(num1, num2);
+	printf("%d\n", result);
 	return (0);
 }
-

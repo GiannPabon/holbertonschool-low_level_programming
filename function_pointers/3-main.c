28bf864b2dfaec9418355f82414e225a28f62520
Giann Pabon <giannalberto2@outlook.com>
2024-11-22 15:06:28 -0400

    main 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,28 +6,24 @@
  *
  * Return: 0 on success, or exits with appropriate status code on error.
  */
-
 int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
-	int result;
-	int (*opr)(int, int);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	opr = get_op_func(argv[2]);
-	if (opr == NULL)
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = opr(num1, num2);
-	printf("%d\n", result);
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
+

32d1547856154f4a370f648eafc543e413596086
Giann Pabon <giannalberto2@outlook.com>
2024-11-24 18:40:57 -0400

    Task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,6 +6,23 @@
  * print_numbers - Prints numbers with a separator.
  * @separator: String to be printed between numbers.
  * @n: Number of integers to print.
+ *
+ * Return: Nothing.
  */
-void print_numbers(const char *separator
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
 

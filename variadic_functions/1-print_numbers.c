cedc60444dd74656834f3e88824676a3c0d69d9a
Giann Pabon <giannalberto2@outlook.com>
2024-11-24 18:38:20 -0400

    Task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Prints numbers with a separator.
+ * @separator: String to be printed between numbers.
+ * @n: Number of integers to print.
+ */
+void print_numbers(const char *separator
+

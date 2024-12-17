8d1afe6fd3242dd7b66db297e5051ec65e9a6785
Giann Pabon <giannalberto2@outlook.com>
2024-12-17 16:35:01 -0400

    vs code

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,8 @@
+#ifndef MAIN_H   /* Header guard to prevent multiple inclusions */
+#define MAIN_H
+
+/* Function prototypes */
+int _putchar(char c);           /* Prototype for _putchar */
+void print_alphabet(void);      /* Prototype for print_alphabet */
+
+#endif

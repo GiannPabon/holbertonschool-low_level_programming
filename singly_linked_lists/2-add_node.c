3b25826a9a8570af9be8ff00675277a6afe3de62
Giann Pabon <giannalberto2@outlook.com>
2025-01-10 09:18:57 -0400

    second task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * _strlen - Calculates the length of a string
+ * @str: The string to measure
+ *
+ * Return: The length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	return (len);
+}
+
+/**
  * add_node - Adds a new node at the beginning of a list_t list
  * @head: Double pointer to the head of the list
  * @str: String to be added to the node
@@ -25,7 +41,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 

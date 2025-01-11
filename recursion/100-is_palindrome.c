0484d35e50c9a5f06d36fa43b2df735ff299a616
Giann Pabon <giannalberto2@outlook.com>
2025-01-10 21:51:29 -0400

    Task 7 palindrome

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')  // Base case: empty string is a palindrome
+		return 1;
+	return check_palindrome(s, 0, _strlen_recursion(s) - 1);
+}
+
+/**
+ * check_palindrome - Recursively checks if string is a palindrome.
+ * @s: The string.
+ * @start: The starting index.
+ * @end: The ending index.
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)  // If all characters have been checked
+		return 1;
+	if (s[start] != s[end])  // If characters don't match
+		return 0;
+	return check_palindrome(s, start + 1, end - 1);  // Recurse with inner substring
+}
+

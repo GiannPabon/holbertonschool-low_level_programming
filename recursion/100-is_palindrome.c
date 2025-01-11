ce6aa94e14b5bd6c701cb08a50b897c333804451
Giann Pabon <giannalberto2@outlook.com>
2025-01-10 21:56:16 -0400

    add strlen to palindrome

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -30,3 +30,16 @@ int is_palindrome(char *s)
 	return check_palindrome(s, 0, _strlen_recursion(s) - 1);
 }
 
+/**
+ * _strlen_recursion - Recursively calculates the length of a string.
+ * @s: The string whose length is to be calculated.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')  /* Base case: end of string */
+		return 0;
+	return 1 + _strlen_recursion(s + 1);  /* Recursive case: move to the next character */
+}
+
